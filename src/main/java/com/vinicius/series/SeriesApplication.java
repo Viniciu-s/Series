005e627991a78368fd2a94670f581346bb4cbf15
Vinicius Vieira <viniciusvsoares00@gmail.com>
2023-12-04 14:10:41 -0300

    adicionando método para pesquisar séries com informações de temporadas e episódios.

--- src/main/java/com/vinicius/series/SeriesApplication.java
+++ src/main/java/com/vinicius/series/SeriesApplication.java
@@ -1,8 +1,6 @@
 package com.vinicius.series;
 
-import com.vinicius.series.model.DadosSerie;
-import com.vinicius.series.service.ConsumoApi;
-import com.vinicius.series.service.ConverteDados;
+import com.vinicius.series.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,7 @@ public class SeriesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
